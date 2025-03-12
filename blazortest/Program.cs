38fc64ab9afc31f9607904d5096b6c9245dc3138
Aaron Sharon Dsouza <aasds@eg.dk>
2025-03-12 16:15:00 +0530

    fix : Configation for Snackbar.

--- blazortest/Program.cs
+++ blazortest/Program.cs
@@ -1,12 +1,21 @@
 using blazortest;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddMudServices();
+//builder.Services.AddMudServices();
+
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.TopCenter; // Center the Snackbar
+    config.SnackbarConfiguration.VisibleStateDuration = 3000; // Adjust duration as needed
+    config.SnackbarConfiguration.HideTransitionDuration = 500;
+    config.SnackbarConfiguration.ShowTransitionDuration = 500;
+});
 
 builder.Services.AddScoped(sp => new HttpClient
 {
